02c8143ecc7fdd10b3214ce5033e216377b3b055
Erkinai <erkinai7175@gmail.com>
2023-01-06 19:20:18 +0300

    first commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <div className="footer">
       <div className="container">
         <div className="top">
-          <img src={logo} alt="/" />
+          <img src={logo} width="30px" height="30px" alt="/" />
           <h3>ROMANIA</h3>
         </div>
       </div>
